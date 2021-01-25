eee1bbab0b9aab0ac06583e3e265af6bc7c713f6
Alice-Rez <reznickova.alice@seznam.cz>
2021-01-25 11:35:56 +0100

    added special file for establishing connection

---
+++ model/connection.js
@@ -0,0 +1,28 @@
+let mongoose = require("mongoose");
+
+let url = process.env.MONGODB_URI;
+
+// we are creating connection function - we will not create new connection for each query, but we will let one for the whole time
+// function checks if there exist some connection and if yes, it will use that.....
+function connect() {
+  return new Promise((resolve, reject) => {
+    if (mongoose.connection.readyState === 1) {
+      resolve();
+    } else {
+      mongoose
+        .connect(url, {
+          useCreateIndex: true,
+          useUnifiedTopology: true,
+          useNewUrlParser: true,
+        })
+        .then(() => {
+          resolve();
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    }
+  });
+}
+
+module.exports = connection;
