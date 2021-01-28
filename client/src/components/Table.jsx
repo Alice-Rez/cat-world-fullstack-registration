b93d307ca52f99f47cec5fc88944eba4101435dc
Alice-Rez <reznickova.alice@seznam.cz>
2021-01-28 21:59:13 +0100

    added illustration to cat lovers

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
+import lovers from "../assets/images/lovers.svg";
 
 export default function Table() {
   const [users, setUsers] = useState([]);
@@ -17,8 +18,15 @@ export default function Table() {
   }, []);
 
   return (
-    <section className="container py-5">
-      <h2 className="display-4 py-3 text-left">Cat Lovers</h2>
+    <section className="container py-5 users">
+      <header className="d-flex align-items-center justify-content-start flex-wrap">
+        <img
+          src={lovers}
+          alt="cat sending hearts in direction of the headline"
+          className="message-image"
+        />
+        <h2 className="display-4 py-5 text-left">Cat Lovers</h2>
+      </header>
       <table className="table table-striped">
         <thead>
           <tr>
