44177662b1295eff4cb0fff0d6369251f91660b6
Alice-Rez <reznickova.alice@seznam.cz>
2021-02-02 14:43:30 +0100

    validationData universal

--- controllers/validControllers.js
+++ controllers/validControllers.js
@@ -1,34 +1,27 @@
 const validateData = (req, res, next) => {
   let { fullName, email, password, uname, newPassword } = req.body;
   if (fullName && email && password && uname) {
-    req
-      .check("fullName", "fullname")
-      .custom((value) => {
-        return value.match(/^[A-Za-z ]+$/);
-      })
-      .trim()
-      .escape();
-    req.check("email", "email").isEmail().trim().escape();
+    req.check("fullName", "fullname").custom((value) => {
+      return value.match(/^[A-Za-z ]+$/);
+    });
+    req.check("email", "email").isEmail();
     req
       .check("password", "password length")
       .isLength({ min: 10 })
       .trim()
       .escape();
-    req.check("uname", "uname").isAlphanumeric().trim().escape();
-  }
-  if (email) {
-    req.check("email", "email").trim().escape();
-  }
-  if (password) {
-    req.check("password", "password").trim().escape();
+    req.check("uname", "uname").isAlphanumeric();
   }
   if (newPassword) {
-    req
-      .check("newPassword", "password length")
-      .isLength({ min: 10 })
-      .trim()
-      .escape();
+    req.check("newPassword", "password length").isLength({ min: 10 });
   }
+  if (email) {
+    req.check("email").normalizeEmail();
+  }
+
+  Object.keys(req.body).map((input) => {
+    req.check(`${input}`).trim().escape();
+  });
 
   let errors = req.validationErrors();
 
